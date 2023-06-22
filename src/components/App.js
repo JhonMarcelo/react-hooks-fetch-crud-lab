283dbc2f6834f0b88c2061ec4e47516c71770642
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-06-22 17:14:15 -0400

    Complete Putting it all Together: React Fetch CRUD Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
@@ -6,10 +6,30 @@ import QuestionList from "./QuestionList";
 function App() {
   const [page, setPage] = useState("List");
 
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((fetchedQuestions) => setQuestions(fetchedQuestions));
+  }, setQuestions);
+
+  function handleDelete(id) {
+    const updatedQuestions = questions.filter((question) => question.id !== id);
+    setQuestions(updatedQuestions);
+  }
+
+  function handleUpdatedQuestions(newQuestion) {
+    setQuestions([...questions, newQuestion]);
+  }
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onUpdatedQuestion={handleUpdatedQuestions} />
+      ) : (
+        <QuestionList questions={questions} deletedQuestion={handleDelete} />
+      )}
     </main>
   );
 }
--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ onUpdatedQuestion }) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -19,7 +19,30 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    // console.log(
+    //   formData.prompt,
+    //   [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+    //   formData.correctIndex
+    // );
+    const newQuestions = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: formData.correctIndex,
+    };
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newQuestions),
+    })
+      .then((r) => r.json())
+      .then((newQuestion) => onUpdatedQuestion(newQuestion));
   }
 
   return (
